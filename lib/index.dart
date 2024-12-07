bce3d2cb1f1b99b62dc399bd6ac084a10ecc4b30
Dylan Toner <dylanjtoner@gmail.com>
2024-12-07 13:46:13 +0000

    update index

--- lib/index.dart
+++ lib/index.dart
@@ -1,8 +1,4 @@
 // Export pages
 export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export 'pages/usb_connection/usb_connection_widget.dart'
-    show USBConnectionWidget;
-export '/pages/bluetooth_connection/bluetooth_connection_widget.dart'
-    show BluetoothConnectionWidget;
 export '/pages/device_page/device_page_widget.dart' show DevicePageWidget;
 export '/pages/settings.dart' show SettingsPage;
